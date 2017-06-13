b7711a439e06c50530a2620369fa29983e8cb777
jankowskirobert <jankowski.r@hotmail.com>
2017-06-13 13:07:55 +0200

    refactor to extract constant

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -4,6 +4,7 @@ import org.hamcrest.Matcher;
 
 public class Server {
 
+	private static final double MAX_LOAD = 100.0d;
 	public double currentProcentageLoad = 0.0d;
 	public double capacity;
 
@@ -15,6 +16,11 @@ public class Server {
 	public boolean contain(Vm theVm) {
 		// TODO Auto-generated method stub
 		return true;
+	}
+
+	public void addVm(Vm vm) {
+		this.currentProcentageLoad = (double)vm.size / (double)this.capacity * MAX_LOAD;
+		
 	}
 
 }
--- src/test/java/edu/iis/mto/serverloadbalancer/CurrentLoadPercentageMatcher.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/CurrentLoadPercentageMatcher.java
@@ -7,11 +7,11 @@ public class CurrentLoadPercentageMatcher extends TypeSafeMatcher<Server>{
 
 	
 	
+	private static final double EPSILON = 0.01d;
 	private double expectedProcentageLoad;
 
 	public CurrentLoadPercentageMatcher(double currentLoad) {
 		this.expectedProcentageLoad = currentLoad;
-		// TODO Auto-generated constructor stub
 	}
 
 	public void describeTo(Description description) {
@@ -25,7 +25,7 @@ public class CurrentLoadPercentageMatcher extends TypeSafeMatcher<Server>{
 	}
 
 	private boolean doubleAreEqual(double d1, double d2) {
-		return d1 == d2 || Math.abs(d1 - d2) < 0.01d;
+		return d1 == d2 || Math.abs(d1 - d2) < EPSILON;
 	}
 
 }
