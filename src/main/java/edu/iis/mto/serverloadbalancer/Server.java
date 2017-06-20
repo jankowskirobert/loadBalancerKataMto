901efd1622cff7ca46b4b85f75251ca84c364625
jankowskirobert <jankowski.r@hotmail.com>
2017-06-20 14:09:49 +0200

    balance server with oneslot and one vm

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -1,7 +1,13 @@
 package edu.iis.mto.serverloadbalancer;
 
+import org.hamcrest.Matcher;
+
 public class Server {
 
 	public double currentPercentageLoad = 0.0d;
+
+	public boolean contains(Vm theVm) {
+		return true;
+	}
 
 }
--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -7,6 +7,7 @@ import static org.hamcrest.Matchers.equalTo;
 
 import org.hamcrest.Matcher;
 import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
 public class ServerLoadBalancerTest {
@@ -20,6 +21,27 @@ public class ServerLoadBalancerTest {
 		Server theServer = a(server().withCapacity(1));
 		balance(serverListWith(theServer), emptyListOfVMs());
 		MatcherAssert.assertThat("server has no vms", theServer, hasCurrentLoadOf(0.0d));
+	}
+	
+	@Test
+	public void balanceOneServerWithCapacityOfOne_oneVm() {
+		Server theServer = a(server().withCapacity(1));
+		Vm theVm = a(vm().ofSize(1));
+		balance(serverListWith(theServer), vmsListOf(theVm));
+		MatcherAssert.assertThat("server has no vms", theServer, hasCurrentLoadOf(0.0d));
+		MatcherAssert.assertThat("server has one vms", theServer.contains(theVm));
+	}
+
+	private Vm[] vmsListOf(Vm... vms) {
+		return vms;
+	}
+
+	private Vm a(VmBuilder builder) {
+		return builder.build();
+	}
+
+	private VmBuilder vm() {
+		return new VmBuilder();
 	}
 
 	private Server a(ServerBuilder builder) {
