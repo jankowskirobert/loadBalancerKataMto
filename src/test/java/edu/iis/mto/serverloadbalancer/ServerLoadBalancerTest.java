7b964fd97c685607565ddef66c76f21098c32fca
jankowskirobert <jankowski.r@hotmail.com>
2017-06-20 13:57:30 +0200

    balance server with no vms

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -4,6 +4,8 @@ package edu.iis.mto.serverloadbalancer;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import org.hamcrest.Matcher;
+import org.hamcrest.MatcherAssert;
 import org.junit.Test;
 
 public class ServerLoadBalancerTest {
@@ -12,5 +14,35 @@ public class ServerLoadBalancerTest {
 		assertThat(true, equalTo(true));
 	}
 
+	@Test
+	public void balanceServerWithNoVMs() {
+		Server theServer = a(server().withCapacity(1));
+		balance(serverListWith(theServer), emptyListOfVMs());
+		MatcherAssert.assertThat("server has no vms", theServer, hasCurrentLoadOf(0.0d));
+	}
+
+	private Server a(ServerBuilder builder) {
+		return builder.build();
+	}
+
+	private CurrentPercentageLoadMatcher hasCurrentLoadOf(double expectedLoad) {
+		return new CurrentPercentageLoadMatcher(expectedLoad);
+	}
+
+	private void balance(Server[] servers, Vm[] vms) {
+		new ServerLoadBalancer().balance(servers, vms);
+	}
+
+	private Vm[] emptyListOfVMs() {
+		return new Vm[]{};
+	}
+
+	private Server[] serverListWith(Server... servers) {
+		return servers;
+	}
+
+	private ServerBuilder server() {
+		return new ServerBuilder();
+	}
 
 }
